49b31d7a0e550feedfd3cfbf01cc0bf6b060597d
Julia B <julia.bohlin@hyperisland.se>
2026-05-26 13:16:13 +0200

    puzzle logic

---
+++ rooms/room-1/scripts/puzzle.js
@@ -0,0 +1,24 @@
+const tokens = document.querySelectorAll('.token')
+const token1 = document.querySelector('.token-1')
+const token2 = document.querySelector('.token-2')
+const token3 = document.querySelector('.token-3')
+
+tokens.forEach(token => {
+    token.addEventListener('click', function () {
+        const current = this.getAttribute('rotation')
+        this.setAttribute('rotation', {
+            y: current.y + 20,
+            x: current.x,
+            z: current.z
+        })
+        function almost(a, b) {
+            return Math.abs(a - b) < 0.1
+        }
+
+        if (almost(token1.getAttribute('rotation').y, 20)
+            && almost(token2.getAttribute('rotation').y, 40)
+            && almost(token3.getAttribute('rotation').y, 60)) {
+            console.log('clear puzzle')
+        }
+    })
+})
