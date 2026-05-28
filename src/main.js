fd1b14963ce308e2d7a9d7944892b075c1da5c32
vickhoff <dvickhoff@gmail.com>
2026-05-28 12:55:56 +0200

    Room 2 (#3)
    * walls and stuff
    
    * correct dimensions
    
    * Correct position of room

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import AFRAME from "aframe";
-
+import "aframe-extras";
 import "./components/corridor/corridor-textures";
 import "./components/corridor/corridor-state";
 import "./components/corridor/door";
