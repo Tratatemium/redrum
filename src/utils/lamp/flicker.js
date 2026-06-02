1b7e78494d3eb192bde953c2e842537ff0c0dd0b
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-06-02 13:34:07 +0200

    Refactor flicker function to improve light cycling and cleanup logic

--- src/utils/lamp/flicker.js
+++ src/utils/lamp/flicker.js
@@ -10,10 +10,18 @@ function createFlicker(lamp) {
   };
 
   let tl = null;
+  let pendingCall = null;
+
+  function killAll() {
+    tl?.kill();
+    pendingCall?.kill();
+    tl = null;
+    pendingCall = null;
+  }
 
   return {
     normal() {
-      tl?.kill();
+      killAll();
       gsap.to({
         duration: 0.2,
         onUpdate() {
@@ -22,7 +30,7 @@ function createFlicker(lamp) {
       });
     },
     off() {
-      tl?.kill();
+      killAll();
       gsap.to({
         duration: 0.2,
         onUpdate() {
@@ -32,62 +40,39 @@ function createFlicker(lamp) {
     },
 
     flicker(duration = 5) {
-      tl?.kill();
+      killAll();
 
       const baseIntensity = cfg.baseVariant.light.intensity ?? 1;
       const endTime = gsap.ticker.time + duration;
 
-      const proxy = { v: baseIntensity };
-      const setLight = () =>
-        applyVariant(lamp, {
-          ...cfg.baseVariant,
-          light: { ...cfg.baseVariant.light, intensity: proxy.v },
-        });
+      function runCycle() {
+        const proxy = { v: baseIntensity };
+        const setLight = () =>
+          applyVariant(lamp, {
+            ...cfg.baseVariant,
+            light: { ...cfg.baseVariant.light, intensity: proxy.v },
+          });
 
-      tl = gsap.timeline({
-        repeat: -1,
-        repeatDelay: cfg.randomizeDelay
-          ? cfg.repeatDelay + Math.random() * 0.8
-          : cfg.repeatDelay,
-        onRepeat() {
-          if (gsap.ticker.time >= endTime) {
-            tl.kill();
-            applyVariant(lamp, cfg.baseVariant);
-            return;
-          }
-          tl.repeatDelay(cfg.repeatDelay + Math.random() * 0.8);
-        },
-      });
-      tl.to(proxy, {
-        v: 0.1,
-        duration: 0.04,
-        onUpdate: setLight,
-      })
-        .to(proxy, {
-          v: 1.4,
-          duration: 0.04,
-          onUpdate: setLight,
-        })
-        .to(proxy, {
-          v: 0,
-          duration: 0.03,
-          onUpdate: setLight,
-        })
-        .to(proxy, {
-          v: 1.6,
-          duration: 0.07,
-          onUpdate: setLight,
-        })
-        .to(proxy, {
-          v: 0.3,
-          duration: 0.04,
-          onUpdate: setLight,
-        })
-        .to(proxy, {
-          v: baseIntensity,
-          duration: 0.1,
-          onUpdate: setLight,
+        tl = gsap.timeline({
+          onComplete() {
+            applyVariant(lamp, VARIANTS.off);
+            if (gsap.ticker.time >= endTime) {
+              applyVariant(lamp, cfg.baseVariant);
+              return;
+            }
+            const delay = cfg.repeatDelay + Math.random() * 0.8;
+            pendingCall = gsap.delayedCall(delay, runCycle);
+          },
         });
+        tl.to(proxy, { v: 0.1, duration: 0.05, onUpdate: setLight })
+          .to(proxy, { v: 1.4, duration: 0.05, onUpdate: setLight })
+          .to(proxy, { v: 0, duration: 0.04, onUpdate: setLight })
+          .to(proxy, { v: 1.6, duration: 0.08, onUpdate: setLight })
+          .to(proxy, { v: 0.3, duration: 0.04, onUpdate: setLight })
+          .to(proxy, { v: baseIntensity, duration: 0.2, onUpdate: setLight });
+      }
+
+      runCycle();
     },
   };
 }
