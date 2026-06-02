90b4681d70c779d4e6e31949e86ef4dbf161a4df
vickhoff <dvickhoff@gmail.com>
2026-06-02 09:46:45 +0200

    Some more logic

--- rooms/room-2/puzzle.js
+++ rooms/room-2/puzzle.js
@@ -1,5 +1,14 @@
 import gsap from 'gsap';
 
+function isSolved() {
+  const pieces = document.querySelectorAll('.puzzle-piece');
+  return [...pieces].every(el => {
+    const deg = THREE.MathUtils.radToDeg(el.object3D.rotation.y);
+    const normalized = ((deg % 360) + 360) % 360;
+    return normalized < 5 || normalized > 355;
+  });
+}
+
 AFRAME.registerComponent('rotate-on-click', {
   schema: {
     degrees: { default: 22.5 },
@@ -9,11 +18,14 @@ AFRAME.registerComponent('rotate-on-click', {
   init: function () {
     this.el.addEventListener('click', () => {
       const current = this.el.object3D.rotation[this.data.axis];
-      console.log(current)
+      console.log( THREE.MathUtils.radToDeg(current))
       gsap.to(this.el.object3D.rotation, {
         [this.data.axis]: current - THREE.MathUtils.degToRad(this.data.degrees),
         duration: 0.5,
-        ease: 'power2.out'
+        ease: 'power2.out',
+        onComplete: () => {
+          if (isSolved()) console.log("solved");
+        }
       });
     });
   }
