c651a28340470f32e52e90d82e4b6d4a34350408
vickhoff <dvickhoff@gmail.com>
2026-06-08 11:16:31 +0200

    Last commit

--- rooms/room-2/puzzle.js
+++ rooms/room-2/puzzle.js
@@ -46,12 +46,6 @@ AFRAME.registerComponent('rotate-on-click', {
           const normalizedRotation = ((rotationRadians % (Math.PI * 2)) + Math.PI * 2) % (Math.PI * 2);
           this.el.object3D.rotation[this.data.axis] = normalizedRotation;
 
-          const pieces = [...document.querySelectorAll('.puzzle-piece')];
-          pieces.forEach((piece, index) => {
-            const degrees = THREE.MathUtils.radToDeg(piece.object3D.rotation[this.data.axis]);
-            console.log(`piece ${index + 1}: ${degrees.toFixed(1)}°`);
-          });
-
           if (isSolved()) {
             solved = true;
             console.log("solved");
